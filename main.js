81efc9e5e9d698900ee9065a8d27906003a4fdee
mohamed eldeeb <73468629+eng-mohamed-eldeeb@users.noreply.github.com>
2022-02-09 23:39:34 +0200

    Starting the mobile version

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 const speakersData = [
-    {
+  {
     name: "Ahmed helmy",
-    img_source: './imgs/speaker1.jpg',
+    img_source: "./imgs/speaker1.jpg",
     occupation: "dolor psumectetur adipisicing elit.",
-    inini: "psum dolor sit amLorem, iet abore nostrum laudantium eaque suscipit cupiditate?"
-},
-    {
+    inini:
+      "psum dolor sit amLorem, iet abore nostrum laudantium eaque suscipit cupiditate?",
+  },
+  {
     name: "Mie Eaz El-deen",
-    img_source: './imgs/speaker2.jpg',
+    img_source: "./imgs/speaker2.jpg",
     occupation: "sit amet consectet dolor, r adipisi",
-    inini: "r sit amet consectetu elit. Laboium eaque suscipit cupiditate?"
-},
-    {
+    inini: "r sit amet consectetu elit. Laboium eaque suscipit cupiditate?",
+  },
+  {
     name: "Maged El-kedwani",
-    img_source: './imgs/speaker3.jpg',
+    img_source: "./imgs/speaker3.jpg",
     occupation: "ipsum dolor, sit ame adipisicing elit.",
-    inini: "ipsum dolcing elit. La laudantium eaque suscipit cupiditate?"
-},
-    {
+    inini: "ipsum dolcing elit. La laudantium eaque suscipit cupiditate?",
+  },
+  {
     name: "Ahmed Eez",
-    img_source: './imgs/speaker4.jpg',
+    img_source: "./imgs/speaker4.jpg",
     occupation: "L amet consectetur adipisicing elit.",
-    inini: "Lorem, ipsum dolor sium eaque suscipit cupiditate?"
-},
-    {
+    inini: "Lorem, ipsum dolor sium eaque suscipit cupiditate?",
+  },
+  {
     name: "Bioumi fouad",
-    img_source: './imgs/speaker5.jpg',
+    img_source: "./imgs/speaker5.jpg",
     occupation: "dolor, sit amepsum onsecteg elit.",
-    inini: "Lorem, ipsum dolor sitntium eaque suscipit cupiditate?"
-},
-    {
+    inini: "Lorem, ipsum dolor sitntium eaque suscipit cupiditate?",
+  },
+  {
     name: "Ahmed saad",
-    img_source: './imgs/speaker6.jpg',
+    img_source: "./imgs/speaker6.jpg",
     occupation: "lor, Lorem ipsum dosipisicing elit.",
-    inini: "psum dolor sit amet itue suscipit cupiditate?"
-},
-]
+    inini: "psum dolor sit amet itue suscipit cupiditate?",
+  },
+];
 
-const ul_ = document.querySelector('.speaker ul');
-
+const ul_ = document.querySelector(".speaker ul");
 
 function speakerCreation(speaker) {
-    const liWithData = `
+  const liWithData = `
         <li>
             <div class="speakerImg ahmed"><img src="${speaker.img_source}" alt=""></div>
             <div class="speakerInfo">
@@ -53,15 +53,16 @@ function speakerCreation(speaker) {
             <li>${speaker.inini}</li>
             </ul>
             </div>
-        </li>`
-        console.log(speaker.name)
-        ul_.innerHTML += liWithData;
-
+        </li>`;
+  ul_.innerHTML += liWithData;
 }
 
-
-for (let i = 0; i < speakersData.length ; i++) {
+if (screen.width >= 768) {
+  for (let i = 0; i < speakersData.length; i++) {
     speakerCreation(speakersData[i]);
-
+  }
+} else {
+  for (let i = 0; i < 2; i++) {
+    speakerCreation(speakersData[i]);
+  }
 }
-
